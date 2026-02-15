d972f7a390f219bf6e78ddf3f9153ae505277dc8
Enric Morella Violeta <144460296+em0rev1@users.noreply.github.com>
2026-02-15 21:11:50 +0100

    Update DOTFILES_DIR path in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOTFILES_DIR="$HOME/dotfiles"
+DOTFILES_DIR="$HOME/.dotfiles"
 
 
 ln -sf "$DOTFILES_DIR/bash/.bashrc" "$HOME/.bashrc"
@@ -8,7 +8,7 @@ ln -sf "$DOTFILES_DIR/bash/.bashrc" "$HOME/.bashrc"
 # ln -sf "$DOTFILES_DIR/zsh/.zshrc" "$HOME/.zshrc"
 # ln -sf "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
 
-ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "$DOTFILES_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 # mkdir -p "$HOME/.config"
 # ln -sf "$DOTFILES_DIR/nvim/.config/nvim" "$HOME/.config/nvim"
